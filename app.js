5129e6164c0cdb50baed85b27b2d45621bd52d49
yryryk <tovchennikov@e1.ru>
2023-02-26 21:10:11 +0500

    feat: добавить контроллеры и роуты для пользователей

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
 const { PORT = 3000 } = process.env;
 const app = express();
 
-mongoose.connect('mongodb://localhost:27017/mydb', {
-// useNewUrlParser: true,
-// useCreateIndex: true,
-//   useFindAndModify: false
-});
+app.use(bodyParser.json()); // для собирания JSON-формата
+app.use(bodyParser.urlencoded({ extended: true })); // для приёма веб-страниц внутри POST-запроса
+
+mongoose.set('strictQuery', true);
+mongoose.connect('mongodb://0.0.0.0:27017/mestodb');
+
+app.use('/users', require('./routes/users'));
+
 app.listen(PORT, () => {
 });
