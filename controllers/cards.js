b70f861db55935ce4f70bdaa3a46eb7fad400062
yryryk <tovchennikov@e1.ru>
2023-02-27 23:10:43 +0500

    feat: добавить populate

--- controllers/cards.js
+++ controllers/cards.js
@@ -2,6 +2,7 @@ const Card = require('../models/card');
 
 module.exports.getCards = (req, res) => {
   Card.find({})
+    .populate('owner')
     .then((cards) => res.send({ data: cards }))
     .catch((err) => res.status(500).send({ message: err.message }));
 };
