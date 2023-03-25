185bf4b51d6dfea62cc48de18b43c148a06f50a4
yryryk <tovchennikov@e1.ru>
2023-03-26 01:58:23 +0500

    fix: 3000

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const NotFoundError = require('./utils/NotFoundError');
 const { URL_REGEX } = require('./utils/constants');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 
-const { PORT = 3001 } = process.env;
+const { PORT = 3000 } = process.env;
 const app = express();
 
 app.use(helmet());
